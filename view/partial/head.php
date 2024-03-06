50843be7fd04b7522af9e3f0ede3070bfb85d425
siamshaeed <siamshaeed@gmail.com>
2024-03-06 21:36:06 +0600

    home page mastaring

---
+++ view/partial/head.php
@@ -0,0 +1,22 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="description" content="Orbitor,business,company,agency,modern,bootstrap4,tech,software">
+    <meta name="author" content="themefisher.com">
+
+    <title>Doctor Managment System | @siamshaeed</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/frontend/images/favicon.ico" />
+
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="assets/frontend/plugins/bootstrap/css/bootstrap.min.css">
+    <!-- Icon Font Css -->
+    <link rel="stylesheet" href="assets/frontend/plugins/icofont/icofont.min.css">
+    <!-- Slick Slider  CSS -->
+    <link rel="stylesheet" href="assets/frontend/plugins/slick-carousel/slick/slick.css">
+    <link rel="stylesheet" href="assets/frontend/plugins/slick-carousel/slick/slick-theme.css">
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="assets/frontend/css/style.css">
+
+</head>
---
+++ view/partial/script.php
@@ -0,0 +1,19 @@
+<!-- Main jQuery -->
+<script src="assets/frontend/plugins/jquery/jquery.js"></script>
+<!-- Bootstrap 4.3.2 -->
+<script src="assets/frontend/plugins/bootstrap/js/popper.js"></script>
+<script src="assets/frontend/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/frontend/plugins/counterup/jquery.easing.js"></script>
+<!-- Slick Slider -->
+<script src="assets/frontend/plugins/slick-carousel/slick/slick.min.js"></script>
+<!-- Counterup -->
+<script src="assets/frontend/plugins/counterup/jquery.waypoints.min.js"></script>
+
+<script src="assets/frontend/plugins/shuffle/shuffle.min.js"></script>
+<script src="assets/frontend/plugins/counterup/jquery.counterup.min.js"></script>
+<!-- Google Map -->
+<script src="assets/frontend/plugins/google-map/map.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA&callback=initMap"></script>
+
+<script src="assets/frontend/js/script.js"></script>
+<script src="assets/frontend/js/contact.js"></script>
