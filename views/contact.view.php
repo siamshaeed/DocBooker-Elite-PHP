2e8cc03556539858d62814fe1af2ba00aba6681a
siamshaeed <siamshaeed@gmail.com>
2024-03-09 18:50:35 +0600

    map

--- views/contact.view.php
+++ views/contact.view.php
@@ -105,8 +105,10 @@
 </section>
 
 
-<div class="google-map ">
-    <div id="map"></div>
+<div class="google-maps ">
+    <div id="">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7301.405456400655!2d90.42801949999999!3d23.793597900000012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1709985674955!5m2!1sen!2sbd" width="1265" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
 </div>
 
 <?php require 'views/partial/footer.php' ?>
